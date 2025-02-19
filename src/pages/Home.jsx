adbed5489ba0fcadf21e49f8f3208825621c7116
LuisIslas <al222310478@gmail.com>
2025-02-19 07:33:29 -0600

    Arreglo de  Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
+import React, { useState } from 'react'; 
 import Register from "../components/login/register";
 
 const Home = () => {
-    
+    const [formularioActivo, setFormularioActivo] = useState("login");
     return (
         <div>
             <div className="formu">
-            <Register />
+                <Register />
             </div>
         </div>
     );
