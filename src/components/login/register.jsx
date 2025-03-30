9616ed1254ac2f491c2226d65757e1ac09a9cec2
Nxlogix <sofiasd121514@gmail.com>
2025-03-30 08:59:17 -0600

    Implementacion de IP en login y register

--- src/components/login/register.jsx
+++ src/components/login/register.jsx
@@ -19,7 +19,7 @@ const Register = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post("http://127.0.0.1:5000/usuario/", registro)
+        axios.post("https://3.143.223.115/usuario/", registro)
         .then(() => alert("Usuario registrado exitosamente"))
         .catch(error => console.error("Error al registrar usuario:", error));
     };
